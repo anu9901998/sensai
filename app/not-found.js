532e6e9cc3a6d0194ffcd429878bfdb2c967e7be
lucky <luckyverma14082004@gmail.com>
2025-07-06 14:36:27 +0530

    added ui

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+      <p className="text-muted-foreground">Sorry, we couldn’t find the page you’re looking for.</p>
+    </div>
+  )
+}
---
+++ components/hero.jsx
@@ -0,0 +1,52 @@
+
+import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Hero = () => {
+    return (
+        <section className='w-full pt-36 md:pt-48 pb-10'>
+            <div className='space-y-6 text-center'>
+                <div className='space-y-6 mx-auto'>
+                    <h1 className='text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title'>
+                        {" "}
+                        Your AI Career Coach for
+                        <br />
+                        Professional Success
+                    </h1>
+                    <p className='mx-auto max-w-[600] text-muted-foreground md:text-xl'>
+                        Advance your career with personalized guidance, interview prep, and
+                        AI-powered tools for job success.
+                    </p>
+                </div>
+                <div className='flex justify-center space-x-4'>
+                    <Link href="/dashboard">
+                        <Button  className={"px-8"}>
+                            Get Started
+                        </Button>
+                    </Link>
+                     <Link href="https://www.google.com">
+                        <Button  className={"px-8"} variant={"outline"}>
+                            Get Started
+                        </Button> 
+                    </Link>
+                </div>
+                <div className='hero-image-wrapper mt-5 md:mt-0'>
+                    <div>
+                        <Image
+                         src={"/banner.jpeg"}
+                         width={1280}
+                         height={720}
+                         alt='Banner Sensai'
+                         className='rounded-lg shadow-2xl border mx-auto'
+                         priority
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
